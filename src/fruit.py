2d5f8f06910e14777889eb44eae4ae32b6666b1f
nxszy <nizinski.szy@gmail.com>
2024-04-14 21:25:27 +0200

    Changed positon generation - now it is impossible for apple to spawn inside the snake (xd).

--- src/fruit.py
+++ src/fruit.py
@@ -4,17 +4,24 @@ import random
 APPLE = pygame.image.load('./assets/apple.png')
 
 class Fruit:
-    def __init__(self, board_max_width: int, board_max_height: int, square_size: int):
-        self.x = random.randint(1, board_max_width - 1)
-        self.y = random.randint(0, board_max_height - 1)
+    def __init__(self, board_max_width: int, board_max_height: int, square_size: int, snake_body):
+
         self.board_max_width = board_max_width
         self.board_max_height = board_max_height
         self.square_size = square_size
+
         self.image = APPLE
+        self.snake_body = snake_body
+
+        self.x, self.y = self.generate_position()
 
     @property
     def pos(self):
         return (self.x, self.y)
+    
+    def generate_position(self):
+        free_squares = [(x, y) for x in range(1, self.board_max_width) for y in range(self.board_max_height) if (x,y) not in [frag.pos for frag in self.snake_body]]
+        return random.choice(free_squares)
 
     def draw(self, screen: pygame.display):
         screen.blit(self.image, (self.y*self.square_size, self.x*self.square_size))
